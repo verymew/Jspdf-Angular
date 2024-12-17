67e9e29a64b40f92a9bf90a52d6d392f328e63c2
Julia Ritter <111842541+verymew@users.noreply.github.com>
2024-12-17 09:07:20 -0300

    Delete projeto_pdf directory

--- projeto_pdf/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { IndexComponent } from './index/index.component';
-
-export const routes: Routes = [
-    { path: '', component: IndexComponent }
-];
